10b6c92ba18b92c45b3613187750413419059333
jaso69 <joseonieva@gmail.com>
2025-02-12 20:57:58 +0100

    uploads

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,11 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
   vite: {
     plugins: [tailwindcss()],
     ssr: {
       external: ["crypto", "fs", "path", "stream", "http", "https", "querystring", "url"],
     },
   },
-
   adapter: cloudflare()
 });
